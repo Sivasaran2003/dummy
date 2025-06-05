1a67ce38305af15e47880dcd6d7b6837645102b1
sivasaran.intern <sivasaran.intern@phonepe.com>
2025-06-05 17:15:44 +0530

    main commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("main branch");
         for(int i = 0; i < 4; i++) {
             System.out.println("learning git");
         }
